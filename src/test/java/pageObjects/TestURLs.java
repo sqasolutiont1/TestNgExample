560a556b832880dc79a7d5c0a07230201f329d14
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-03-10 22:07:50 -0600

    Initial commit

---
+++ src/test/java/pageObjects/TestURLs.java
@@ -0,0 +1,5 @@
+package pageObjects;
+
+public class TestURLs {
+    public static String MainURL = "http://authenticgoods.co/wrapbootstrap/themes/neuboard-v1.4.3/Angular_full_version/index.html#";
+}
